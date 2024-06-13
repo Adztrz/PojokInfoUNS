f882542c0630799194a62952fd7745960580be38
Aditia Nugraha <adztrz1@gmail.com>
2024-06-13 15:54:39 +0700

    Update  Manage User Identation

--- resources/views/livewire/users/index.blade.php
+++ resources/views/livewire/users/index.blade.php
@@ -74,10 +74,10 @@
 	                User
                 @endif
               </td>
-              <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+              <td class="px-6 py-4 whitespace-nowrap text-left text-sm font-medium">
                 <a href="{{ route('users.edit', ['user' => $user->id ]) }}" class="text-indigo-600 hover:text-indigo-900">Edit</a>
               </td>
-              <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+              <td class="px-6 py-4 whitespace-nowrap text-left text-sm font-medium">
                 <a href="#" class="text-red-600 hover:text-red-900">Delete</a>
               </td>
             </tr>
